3d0d1c54cc4b2881fe71da31d33cd1416ca4fb4c
FelpsPrograms <felipe.vasel30052023@gmail.com>
2024-09-02 13:50:41 -0300

    Adição deleteSQL()

--- relatorio/src/main/java/com/systextil/relatorio/savedQuery/SavedQueryController.java
+++ relatorio/src/main/java/com/systextil/relatorio/savedQuery/SavedQueryController.java
@@ -2,7 +2,9 @@ package com.systextil.relatorio.savedQuery;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +34,13 @@ public class SavedQueryController {
         savedQueryRepository.save(savedQuery);
 
         return ResponseEntity.created(null).body(savedQuery);
+    }
+    
+    @DeleteMapping("delete/{queryName}")
+    public ResponseEntity<Void> deleteSQL(@PathVariable(value = "queryName") String queryName) {
+    	savedQueryRepository.deleteByQueryName(queryName);
+    	
+    	return ResponseEntity.noContent().build();
     }
 
 }
