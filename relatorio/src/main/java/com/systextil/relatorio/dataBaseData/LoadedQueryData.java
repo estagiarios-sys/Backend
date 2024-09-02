f35d63b948b2be0696ae5f9d40f0855f1adc8c19
FelpsPrograms <felipe.vasel30052023@gmail.com>
2024-09-02 09:54:38 -0300

    LoadedQueryData.columns -> columnsBanco

--- relatorio/src/main/java/com/systextil/relatorio/dataBaseData/LoadedQueryData.java
+++ relatorio/src/main/java/com/systextil/relatorio/dataBaseData/LoadedQueryData.java
@@ -3,6 +3,6 @@ package com.systextil.relatorio.dataBaseData;
 import java.util.ArrayList;
 
 record LoadedQueryData(
-	ArrayList<String> columns,
+	ArrayList<String> columnsBanco,
 	ArrayList<Object[]> foundObjects
 ) {}
