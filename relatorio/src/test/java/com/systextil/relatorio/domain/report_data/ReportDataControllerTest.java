62c12c13fc6a4d9fe73623f97096221a221b632a
FelpsPrograms <felipe.vasel30052023@gmail.com>
2024-11-05 10:43:47 -0300

    Teste unidade domain/report_data controller

--- relatorio/src/test/java/com/systextil/relatorio/domain/report_data/ReportDataControllerTest.java
+++ relatorio/src/test/java/com/systextil/relatorio/domain/report_data/ReportDataControllerTest.java
@@ -1,16 +1,103 @@
 package com.systextil.relatorio.domain.report_data;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class ReportDataControllerTest {
 
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private JacksonTester<QueryData> requestJson;
+	
+	@Autowired
+	private JacksonTester<Object[]> responseJson;
+	
+	@MockBean
+	private ReportDataService service;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("getQueryReturn: response = bad request")
+	void cenario1() throws Exception {
+		MockHttpServletResponse response = mvc
+				.perform(post("/report-data"))
+				.andReturn()
+				.getResponse();
+		
+		assertEquals(400, response.getStatus());
 	}
 
+	@Test
+	@DisplayName("getQueryReturn: response = ok com dados")
+	void cenario2() throws Exception {
+		Object[] queryReturn = {"query", "query", List.of(), List.of(), Map.of()};
+
+		when(service.getQueryReturn(any())).thenReturn(queryReturn);
+
+		MockHttpServletResponse response = mvc
+				.perform(post("/report-data")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(requestJson.write(
+								new QueryData("TABELA", List.of(new QueryDataColumn("NOME", "APELIDO")), List.of(), "", List.of(), Map.of())
+						).getJson())
+				)
+				.andReturn().getResponse();
+
+		String expectedQueryReturn = responseJson.write(queryReturn).getJson();
+		
+		assertEquals(200, response.getStatus());
+		assertEquals(expectedQueryReturn, response.getContentAsString());
+	}
+	
+	@Test
+	@DisplayName("getQueryAnalysis: response = bad request")
+	void cenario3() throws Exception {
+		MockHttpServletResponse response = mvc
+				.perform(post("/report-data/analyze"))
+				.andReturn()
+				.getResponse();
+		
+		assertEquals(400, response.getStatus());
+	}
+	
+	@Test
+	@DisplayName("getQueryAnalysis: response = ok com dados")
+	void cenario4() throws Exception {
+		when(service.getQueryAnalysis(any())).thenReturn(10.5);
+		
+		MockHttpServletResponse response = mvc
+				.perform(post("/report-data/analyze")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(requestJson.write(
+								new QueryData("TABELA", List.of(new QueryDataColumn("NOME", "APELIDO")), List.of(), "", List.of(), Map.of())
+						).getJson())
+				)
+				.andReturn().getResponse();
+		
+		String expectedQueryAnalysis = "10.5";
+		
+		assertEquals(200, response.getStatus());
+		assertEquals(expectedQueryAnalysis, response.getContentAsString());
+	}
 }
