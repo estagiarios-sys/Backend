f8a3ac394465a55bec02132aeca11c0ba4bdaf7e
FelpsPrograms <felipe.vasel30052023@gmail.com>
2024-11-29 11:25:04 -0300

    Teste unidade domain/pdf repository
    Atualização

--- relatorio/src/test/java/com/systextil/relatorio/domain/pdf/PdfRepositoryTest.java
+++ relatorio/src/test/java/com/systextil/relatorio/domain/pdf/PdfRepositoryTest.java
@@ -5,23 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 /**
  * Classe configurada para usar o banco em memória H2
  */
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.AUTO_CONFIGURED)
-@TestInstance(Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class PdfRepositoryTest {
 
 	@Autowired
@@ -30,30 +29,19 @@ class PdfRepositoryTest {
 	@Autowired
 	private TestEntityManager entityManager;
 	
-	private Long stId = 1L;
-	private Long ndId = 2L;
-	private Long rdId = 3L;
-	
 	@BeforeEach
 	void setUp() {
-		Pdf newestPdf = new Pdf("pdf mais novo", LocalDateTime.of(1, 1, 1, 1, 1));
+		Pdf oldestPdf = new Pdf("pdf mais velho", LocalDateTime.of(1, 1, 1, 1, 1));
 		Pdf middlePdf = new Pdf("pdf do meio", LocalDateTime.of(2, 2, 2, 2, 2));
-		Pdf oldestPdf = new Pdf("pdf mais velho", LocalDateTime.of(3, 3, 3, 3, 3));
-		
-		newestPdf.update(LocalDateTime.of(1, 1, 1, 1, 1), "caminho1");
+		Pdf newestPdf = new Pdf("pdf mais novo", LocalDateTime.of(3, 3, 3, 3, 3));
+
+		oldestPdf.update(LocalDateTime.of(1, 1, 1, 1, 1), "caminho1");
 		middlePdf.update(LocalDateTime.of(2, 2, 2, 2, 2), "caminho2");
-		oldestPdf.update(LocalDateTime.of(3, 3, 3, 3, 3), "caminho3");
+		newestPdf.update(LocalDateTime.of(3, 3, 3, 3, 3), "caminho3");
 		
-		entityManager.persist(newestPdf);
-		entityManager.persist(middlePdf);
 		entityManager.persist(oldestPdf);
-	}
-	
-	@AfterEach
-	void incrementId() {
-		stId += 3L;
-		ndId += 3L;
-		rdId += 3L;
+		entityManager.persist(middlePdf);
+		entityManager.persist(newestPdf);
 	}
 	
 	@Test
@@ -61,9 +49,9 @@ class PdfRepositoryTest {
 	void cenario1() {
 		List<PdfListing> pdfsList = repository.findAttributesForList();
 		
-		PdfListing firstExpectedPdf = new PdfListing(rdId, "pdf mais velho", LocalDateTime.of(3, 3, 3, 3, 3), LocalDateTime.of(3, 3, 3, 3, 3), "CONCLUIDO");
-		PdfListing secondExpectedPdf = new PdfListing(ndId, "pdf do meio", LocalDateTime.of(2, 2, 2, 2, 2), LocalDateTime.of(2, 2, 2, 2, 2), "CONCLUIDO");
-		PdfListing thirdExpectedPdf = new PdfListing(stId, "pdf mais novo", LocalDateTime.of(1, 1, 1, 1, 1), LocalDateTime.of(1, 1, 1, 1, 1), "CONCLUIDO");
+		PdfListing firstExpectedPdf = new PdfListing(3L, "pdf mais novo", LocalDateTime.of(3, 3, 3, 3, 3), LocalDateTime.of(3, 3, 3, 3, 3), "CONCLUIDO");
+		PdfListing secondExpectedPdf = new PdfListing(2L, "pdf do meio", LocalDateTime.of(2, 2, 2, 2, 2), LocalDateTime.of(2, 2, 2, 2, 2), "CONCLUIDO");
+		PdfListing thirdExpectedPdf = new PdfListing(1L, "pdf mais velho", LocalDateTime.of(1, 1, 1, 1, 1), LocalDateTime.of(1, 1, 1, 1, 1), "CONCLUIDO");
 		
 		assertEquals(List.of(firstExpectedPdf, secondExpectedPdf, thirdExpectedPdf), pdfsList);
 	}
@@ -73,13 +61,13 @@ class PdfRepositoryTest {
 	void cenario2() {
 		Long id = repository.getOldestEntry();
 		
-		assertEquals(stId, id);
+		assertEquals(1L, id);
 	}
 	
 	@Test
 	@DisplayName("findPathById")
 	void cenario3() {
-		String path = repository.findPathById(stId);
+		String path = repository.findPathById(1L);
 		
 		assertEquals("caminho1", path);
 	}
