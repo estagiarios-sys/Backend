ff0d8db46a6eb4c56b5a2687d6a5b2f3fa11ad12
FelpsPrograms <felipe.vasel30052023@gmail.com>
2024-12-04 09:17:46 -0300

    Teste unidade [...] ReportDataProcessor
    domain/report_data

---
+++ relatorio/src/test/java/com/systextil/relatorio/domain/report_data/ReportDataProcessorTest.java
@@ -0,0 +1,65 @@
+package com.systextil.relatorio.domain.report_data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.systextil.relatorio.domain.Totalizer;
+
+@SpringBootTest
+class ReportDataProcessorTest {
+
+	@Autowired
+	private ReportDataProcessor reportDataProcessor;
+	
+	@Test
+	@DisplayName("joinColumnsAndTotalizersResult")
+	void cenario1() {
+		Map<String, String> columnsNameAndNickName = new LinkedHashMap<>();
+		columnsNameAndNickName.put("NOME", "NOME_CLIENTE");
+		columnsNameAndNickName.put("IDADE", "");
+		columnsNameAndNickName.put("SALARIO", "PAGAMENTO_MENSAL");
+		
+		List<String> totalizersResults = List.of("100", "50000");
+		
+		Map<String, Totalizer> totalizers = new LinkedHashMap<>();
+		totalizers.put("IDADE", Totalizer.SUM);
+		totalizers.put("SALARIO", Totalizer.SUM);
+		
+		ReportData reportData = new ReportData(columnsNameAndNickName, null, totalizersResults);
+		
+		Map<String, String> columnsAndTotalizersResult = reportDataProcessor.joinColumnsAndTotalizersResult(reportData, totalizers);
+		
+		Map<String, String> expectedColumnsAdnTotalizersResult = new LinkedHashMap<>();
+		expectedColumnsAdnTotalizersResult.put("IDADE", "Soma: 100");
+		expectedColumnsAdnTotalizersResult.put("PAGAMENTO_MENSAL", "Soma: 50000");
+		
+		assertEquals(expectedColumnsAdnTotalizersResult, columnsAndTotalizersResult);
+	}
+
+	@Test
+	@DisplayName("toColumnsNameOrNickName")
+	void cenario2() {
+		Map<String, String> columnsNameAndNickName = new LinkedHashMap<>();
+		columnsNameAndNickName.put("NOME", "NOME_CLIENTE");
+		columnsNameAndNickName.put("IDADE", null);
+		columnsNameAndNickName.put("SALARIO", "PAGAMENTO_MENSAL");
+		
+		List<String> columnsNameOrNickName = reportDataProcessor.toColumnsNameOrNickName(columnsNameAndNickName);
+		
+		List<String> expectedColumnsNameOrNickName = new ArrayList<>();
+		expectedColumnsNameOrNickName.add("NOME_CLIENTE");
+		expectedColumnsNameOrNickName.add("IDADE");
+		expectedColumnsNameOrNickName.add("PAGAMENTO_MENSAL");
+		
+		assertEquals(expectedColumnsNameOrNickName, columnsNameOrNickName);
+	}
+}
