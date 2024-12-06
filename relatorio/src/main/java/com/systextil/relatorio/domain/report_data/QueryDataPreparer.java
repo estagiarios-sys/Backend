b981ff6e802e6c7d4f061cb43f662b22db1ed351
FelpsPrograms <felipe.vasel30052023@gmail.com>
2024-12-06 17:09:19 -0300

    Organização métodos [...] QueryDataPreparer
    domain/report_data

--- relatorio/src/main/java/com/systextil/relatorio/domain/report_data/QueryDataPreparer.java
+++ relatorio/src/main/java/com/systextil/relatorio/domain/report_data/QueryDataPreparer.java
@@ -65,7 +65,29 @@ class QueryDataPreparer {
     	return joinedColumnsNameAndNickName;
     }
 	
-	String findJoinsByTable(String table, String comparedTable, List<RelationshipData> relationshipsData) {
+	private String compareToMainTable(String mainTable, String firstTable, String lastTable, List<RelationshipData> relationshipsData) {
+		if (!firstTable.equals(mainTable)) {
+			return findJoinsByTable(lastTable, mainTable, relationshipsData);
+		}
+		return "";
+	}
+	
+	private String compareToPreviousTablesPairs(Pattern lastTablePattern, int mainIndex, String firstTable, String lastTable, List<String> tablesPairs, List<RelationshipData> relationshipsData) {
+		StringBuilder joinBuilder = new StringBuilder();
+		
+		for (int secondaryIndex = 0; secondaryIndex < mainIndex; secondaryIndex++) {			
+			Matcher lastTableMatcher = lastTablePattern.matcher(tablesPairs.get(secondaryIndex));
+			lastTableMatcher.find();
+			String previousLastTable = lastTableMatcher.group();
+			
+			if (!firstTable.equals(previousLastTable)) {
+				joinBuilder.append(findJoinsByTable(lastTable, previousLastTable, relationshipsData));
+			}
+		}
+		return joinBuilder.toString();
+	}
+	
+	private String findJoinsByTable(String table, String comparedTable, List<RelationshipData> relationshipsData) {
 		StringBuilder joinBuilder = new StringBuilder();
 		Pattern pattern = Pattern.compile(table + " e " + comparedTable);
     	
@@ -81,26 +103,4 @@ class QueryDataPreparer {
     	}
     	return joinBuilder.toString();
     }
-	
-	String compareToMainTable(String mainTable, String firstTable, String lastTable, List<RelationshipData> relationshipsData) {
-		if (!firstTable.equals(mainTable)) {
-			return findJoinsByTable(lastTable, mainTable, relationshipsData);
-		}
-		return "";
-	}
-	
-	String compareToPreviousTablesPairs(Pattern lastTablePattern, int mainIndex, String firstTable, String lastTable, List<String> tablesPairs, List<RelationshipData> relationshipsData) {
-		StringBuilder joinBuilder = new StringBuilder();
-		
-		for (int secondaryIndex = 0; secondaryIndex < mainIndex; secondaryIndex++) {			
-			Matcher lastTableMatcher = lastTablePattern.matcher(tablesPairs.get(secondaryIndex));
-			lastTableMatcher.find();
-			String previousLastTable = lastTableMatcher.group();
-			
-			if (!firstTable.equals(previousLastTable)) {
-				joinBuilder.append(findJoinsByTable(lastTable, previousLastTable, relationshipsData));
-			}
-		}
-		return joinBuilder.toString();
-	}
 }
